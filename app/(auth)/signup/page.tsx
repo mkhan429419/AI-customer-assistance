4f0284309be4d1b4180bae99e1a077c11432dc03
Maheen Akhtar Khan <116183925+mkhan429419@users.noreply.github.com>
2024-08-06 19:03:15 +0400

    supabase auth implemented

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {SignUpForm} from "./components/SignUpForm";
+
+const SignupPage = () => {
+  return (
+    <div className="flex h-svh items-center">
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default SignupPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import LoginButton from "@/components/LoginLogoutButton";
+import UserGreetText from "@/components/UserGreetText";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
       <h1>Lets build!</h1>
+      <UserGreetText/>
+      <LoginButton/>
     </main>
   );
 }
