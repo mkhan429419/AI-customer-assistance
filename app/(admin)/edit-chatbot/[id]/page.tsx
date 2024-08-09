56610f3c08734184db312221027024a2591f950f
javeria2108 <javeriazh@gmail.com>
2024-08-09 08:21:49 +0500

    update chatbot name and add characteristic form

--- app/(admin)/edit-chatbot/[id]/page.tsx
+++ app/(admin)/edit-chatbot/[id]/page.tsx
@@ -1,7 +1,12 @@
 "use client";
+import { GET_CHATBOT_BY_ID } from "@/app/api/graphql/queries/queries";
+import Avatar from "@/components/Avatar";
+import Characteristic from "@/components/Characteristic";
 import { BASE_URL } from "@/components/graphql/apolloCient";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { GetChatbotByIdResponse, GetChatbotByIdVariables } from "@/types/types";
+import { useQuery } from "@apollo/client";
 import { Copy } from "lucide-react";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
@@ -9,6 +14,17 @@ import { toast } from "sonner";
 
 const EditChatbot = ({ params: {id} }: { params: { id: string } }) => {
   const [url, setUrl] = useState("");
+  const [chatbotName, setChatbotName]=useState<string>("")
+  const [newCharacteristic, setNewCharacteristic]=useState<string>("")
+
+  const{data,loading,error}=useQuery<GetChatbotByIdResponse, GetChatbotByIdVariables>(GET_CHATBOT_BY_ID,{
+    variables:{id}
+  })
+
+  useEffect(()=>{
+    if(data){
+      setChatbotName(data.chatbots.name)
+    }}, [data])
 
   useEffect(() => {
     const url = `${BASE_URL}/chatbot/${id}`;
@@ -42,7 +58,49 @@ const EditChatbot = ({ params: {id} }: { params: { id: string } }) => {
           <Copy className="w-4 h-4" />
         </Button>
         </div>
+
       </div>
+      <section className="relative mt-5 bg-white p-5 md:p-10 rounded-lg">
+        <Button variant="destructive" className="absolute top-2 right-2 h-8 w-2">X</Button>
+        <div className="flex space-x-4">
+          <Avatar seed={chatbotName}/>
+          <form 
+          // onSubmit={handleUpdateChatbot}
+           className="flex flex-1 space-x-2 items-center">
+            <Input
+            value={chatbotName}
+            onChange={(e)=>setChatbotName(e.target.value)}
+            placeholder={chatbotName}
+            className="w-full border-none bg-transparent text-xl font-bold"
+            required
+            />
+            <Button type="submit" disabled={!chatbotName}>
+              Update
+            </Button>
+          </form>
+        </div>
+        <h2 className="text-xl font-bold mt-10">Heres what your AI knows...</h2>
+        <p>Your chatbot with your customers and users</p>
+        <div>
+          <form>
+            <Input 
+              type="text"
+              placeholder="Example: if customer asks for prices, provide pricing page: www.example.com/pricing"
+              value={newCharacteristic}
+              onChange={(e)=>setNewCharacteristic(e.target.value)}/>
+              <Button type="submit" disabled={!newCharacteristic}>Add</Button>
+          </form>
+          <ul className="flex flex-wrap-reverse gap-5">
+            {data?.chatbots.chatbot_characteristics.map((charateristic)=>(
+               <Characteristic
+               key={charateristic.id}
+               characteristic={charateristic}
+               />
+            ))}
+          </ul>
+
+        </div>
+      </section>
     </div>
   );
 };
---
+++ components/Characteristic.tsx
@@ -0,0 +1,14 @@
+import { ChatbotCharacteristic } from '@/types/types'
+import { OctagonX } from 'lucide-react'
+import React from 'react'
+
+const Characteristic = ({characteristic}:{characteristic: ChatbotCharacteristic}) => {
+  return (
+    <li className='relative p-10 bg-white border rounded-md'>
+        {characteristic.content}
+        <OctagonX className='w-6 h-6 text-white fill-red-500 absolute top-1 right-1 cursor-pointer hover:opacity-50'/>
+    </li>
+  )
+}
+
+export default Characteristic
