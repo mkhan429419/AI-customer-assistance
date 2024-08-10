653354350c7944c2c0bffb59b3a6a57fe98e69f3
Maheen Akhtar Khan <116183925+mkhan429419@users.noreply.github.com>
2024-08-10 19:12:55 +0400

    home + sign-up + login +loading pages created

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,24 +1,29 @@
-import Header from '@/components/Header';
-import Sidebar from '@/components/Sidebar';
-import React from 'react'
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-type Props = {}
-
-const AdminLayout = ({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) => {
+async function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { userId } = await auth();
+  if (!userId) {
+    return redirect("/login");
+  }
   return (
-    <div className='flex flex-col flex-1'>
-        <Header/>
-        <div className='flex flex-col flex-1 lg:flex-row bg-gray-100'>
-            <Sidebar/>  
-            <div className='flex-1 flex justify-center lg:justify-start items-start max-w-5xl mx-auto w-full'>  {children}</div>
+    <div className="flex flex-col flex-1">
+      <Header />
+      <div className="flex flex-col flex-1 lg:flex-row bg-gray-100">
+        <Sidebar />
+        <div className="flex-1 flex justify-center lg:justify-start items-start max-w-5xl mx-auto w-full">
+          {" "}
+          {children}
         </div>
-      
+      </div>
     </div>
-  )
+  );
 }
 
-export default AdminLayout
+export default AdminLayout;
--- app/(admin)/page.tsx
+++ app/(admin)/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>lets goo</h1>
+    <main className="p-10 bg-white m-10 rounded-md w-full">
+      <h1 className="text-4xl font-light">
+        Welcome to{" "}
+        <span className="text-[#64B5F5] font-semibold">Assist AI</span>
+      </h1>
+      <h2 className="mt-2 mb-10">
+        Your customisable AI chat agent that helps you manage your customer
+        conversations.
+      </h2>
+      <Link href="/create-chatbot">
+        <Button className="bg-[#64B5F5]">
+          Let's get started by creating your first chatbot
+        </Button>
+      </Link>
     </main>
   );
 }
